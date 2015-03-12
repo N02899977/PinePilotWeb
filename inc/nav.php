fb4c23a1689e278f06de9afd911644ea4d61b3b2
unknown <Heidi@.(none)>
2015-03-12 16:17:09 -0400

    test commit

--- inc/nav.php
+++ inc/nav.php
@@ -19,7 +19,7 @@
                     <a href="about.php">About</a>
                 </li>
                 <li>
-                    <a href="#">Services</a>
+                    <a href="services.php">Services</a>
                 </li>
                 <li>
                     <a href="contact.php">Contact</a>
